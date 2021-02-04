101639a8ecd477ce571f092b97e982268af8b08b
Raffaele Pizzari <raffaele.pizzari@gmail.com>
2021-02-04 17:45:52 +0100

    feat(challenge): [2021-02-03] Challenge solution

--- challenges/2021-02-03/pixari/index.js
+++ challenges/2021-02-03/pixari/index.js
@@ -2,10 +2,13 @@
 const asc = (a, b) => a - b;
 const desc = (a, b) => b - a;
 const sortFn = k => Math.sign(k) > 0 ? asc : desc;
-const sort = (arr, direction) => arr.sort(sortFn(direction));
+const sort = (direction) => (arr) => arr.sort(sortFn(direction));
 
 const steps = (start, end, step = 1) => Math.floor((end - start) / Math.abs(step)) + 1;
-const range = (start, end, step = 1) => sort(Array(steps(start, end, step)).fill().map((_, idx) => idx * Math.abs(step) + start), step);
+const rangedArray = (start, end, step = 1) => Array(steps(start, end, step)).fill();
+
+const range = (start, end, step = 1) => sort(step)(rangedArray(start, end, step).map((_, idx) => idx * Math.abs(step) + start));
+
 const sum = (arr) => arr.reduce((acc, curr) => curr+acc, 0);
 
 module.exports = {
