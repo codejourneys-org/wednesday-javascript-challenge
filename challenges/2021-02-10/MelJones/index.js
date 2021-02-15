152075e643085a61889b71e14f233318bf452d63
Mel <jones.mel@gmail.com>
2021-02-15 16:15:16 +0100

    week 2 solution complete

--- challenges/2021-02-10/MelJones/index.js
+++ challenges/2021-02-10/MelJones/index.js
@@ -5,6 +5,7 @@ class Group {
 
   add(value) {
     if (!this.collection.includes(value)) {
+      // console.log(`add ${value}`) // to test
       return this.collection.push(value)
     }
   }
@@ -16,28 +17,83 @@ class Group {
   has(value) {
     return this.collection.includes(value)
   }
-
-  static from(obj) {
-    let fromGroup = new Group()
-
-    for (let value of obj) {
-      fromGroup.add(value)
-    }
-    // help!? how do I target add from a static method?
-  }
 }
-const thing = new Group()
-thing.add(10)
-console.log(thing.collection)
-thing.delete(10)
-console.log(thing.has(10))
+
+// // uncomment to test class Group
+// const thing = new Group()
+// thing.add(10)
+// console.log(thing.collection)
+// thing.delete(10)
+// console.log(thing.has(10))
+
+Group.from = function (obj) {
+  let group = new this()
+  for (let value of obj) {
+    group.add(value)
+  }
+  console.log(group.collection)
+  console.log(group.has(10)) // → true
+  console.log(group.has(30)) // → false
+
+  group.add(10)
+  group.delete(10)
+
+  console.log(group.has(10)) // → false
+}
 
 let group = Group.from([10, 20])
-console.log(group.collection)
 
-console.log(group.has(10)) // → true
-console.log(group.has(30)) // → false
-group.add(10)
-group.delete(10)
+// Notes,
+// I don't still don't understand static function yet
+//
+// But I do understand that:
+// they can be writtern using the static keyword or,
+// by assigning them directly as I have above
+//
+// the value of `this` in a static method is the class constructor itself
+// for example the `this` in the from static function is `Group`
+// there call `new this()` from within the static methods
+//
+// Some extra notes on classes in JavaScript
+//
+// everything in JS is an object,
+// compared to other programming languages class is JS are just syntax sugar and not real classes
+//
+// but essentially a class in JS is just a kind of function that creates a blueprint for an object
 
-console.log(group.has(10)) // → false
+// read more here
+// https://javascript.info/class
+
+// /////////////////////////////////
+// /////////////////////////////////
+// Some History of JavaScript
+// it was original written as a schema (OOP) language,
+// but then the builder boss asked him to make it more like Java (FP)
+
+// /////////////////////////////////
+// JS is an OOP & FP language --weird!
+// Because JavaScript is not just OOP language
+// it's also a functional programming language
+
+// Higher Order Functions are thanks to Functional programming
+
+// JS gets confusing and messy when you combine the two,
+// and you can run into errors because its OOP clashing with FP
+// `this` keyword comes from OOP
+// in FP there is no such thing as `this`
+
+//
+
+// The FP part of JS allows you to save an OOP method as a variable, cool! (& not normal compared to other languages)
+// But that means it stops being a method connected to an object, oh no!
+// because in FP there is no such thing as `this`!
+//
+//
+// It becomes a function - and so when you call it, `this` no longer references the object
+// it's lost it's connection! it stops being a metho connected to an object
+// you can fix this using the `bind()` method and binding the object!
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
+
+// Remember
+// `this` refers to the context where the function is being called
+// not where the function was defined
